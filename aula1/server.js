ee5e826a301c2d83fcb315690092709ba7616b81
Tiago Santos <tiago881@gmail.com>
2021-09-02 12:02:43 -0300

    cria uma função para o servidor subir na porta 3000

--- aula1/server.js
+++ aula1/server.js
@@ -4,3 +4,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello, world');
 });
+
+const PORT = 3000;
+const HTTP_OK = 'online';
+
+app.listen(PORT, () => console.log(`${HTTP_OK} -> http://localhost:3000`));
