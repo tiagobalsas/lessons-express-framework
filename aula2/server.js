7f4cb6b835ac61745b9bd97c7e750f4b1fde504b
Tiago Santos <tiago881@gmail.com>
2021-09-03 00:07:45 -0300

    retorna o valor do parametro da url

---
+++ aula2/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+  <body style="background-color:AliceBlue;">
+    <form action="/" method="POST">
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname" style="background-color:#c1c1c1;"><br><br>
+      <button class="button button1" style="color:DarkGreen; font-size: 18px;">Enviar</button>
+    </form>
+  </body>
+  `);
+});
+
+app.get('/feedback/:id?', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.post('/formulario', (req, res) => {
+  res.send('Formulário recebido com sucesso');
+});
+
+const PORT = 3000;
+const HTTP_OK = 'online';
+
+app.listen(PORT, () => console.log(`${HTTP_OK} -> http://localhost:3000`));
