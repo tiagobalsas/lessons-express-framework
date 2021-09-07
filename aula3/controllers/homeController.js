edd2d51437707cbde48c084ca713645c4a19dc40
Tiago Santos <tiago881@gmail.com>
2021-09-07 11:47:18 -0300

    Cria controller paginalInicial e exporta

---
+++ aula3/controllers/homeController.js
@@ -0,0 +1,11 @@
+exports.paginaInicial = (req, res)=>{
+  res.send(`
+  <body style="background-color:AliceBlue;">
+    <form action="/" method="POST">
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname" style="background-color:#c1c1c1;"><br><br>
+      <button class="button button1" style="color:DarkGreen; font-size: 18px;">Enviar</button>
+    </form>
+  </body>
+  `);
+}
