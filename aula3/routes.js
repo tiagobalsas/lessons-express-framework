df59833d7cba5a6e9e8fa552ce170dec7c013c7c
Tiago Santos <tiago881@gmail.com>
2021-09-07 12:10:22 -0300

    Importa contatoController e cria rota /contato

--- aula3/routes.js
+++ aula3/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./controllers/homeController');
+const contatoController = require('./controllers/contatoController');
 
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.msgFeedback);
+
+route.get('/contato', contatoController.homeContato);
 
 module.exports = route;
