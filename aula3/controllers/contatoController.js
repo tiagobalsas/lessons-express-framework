87a197984979b1f55e614ffd998248afc9e36bac
Tiago Santos <tiago881@gmail.com>
2021-09-07 12:08:58 -0300

    Cria controller para rota contato

---
+++ aula3/controllers/contatoController.js
@@ -0,0 +1,11 @@
+exports.homeContato = (req, res) => {
+  res.send(
+    `
+      <h1>Seja bem vindo a página de contato</h1>
+      </br>
+      </br>
+      </br>
+      <h3>Obrigado pela preferência</h3>
+    `
+  );
+};
