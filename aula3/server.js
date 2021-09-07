d22fb014af0210dfd6db4bc7063eab702c67b38b
Tiago Santos <tiago881@gmail.com>
2021-09-07 11:41:50 -0300

    Update

--- aula3/server.js
+++ aula3/server.js
@@ -1,31 +1,9 @@
 const express = require('express');
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send(`
-  <body style="background-color:AliceBlue;">
-    <form action="/" method="POST">
-      <label for="fname">First name:</label>
-      <input type="text" id="fname" name="fname" style="background-color:#c1c1c1;"><br><br>
-      <button class="button button1" style="color:DarkGreen; font-size: 18px;">Enviar</button>
-    </form>
-  </body>
-  `);
-});
-
-app.get('/feedback/:id?', (req, res) => {
-  res.send(req.params.id);
-});
-
-app.get('/formulario', (req, res) => {
-  res.send(req.query);
-});
-
-app.post('/', (req, res) => {
-  res.send(`O meu primeiro nome é: ${req.body.fname}`);
-});
 
 const PORT = 3000;
 const HTTP_OK = 'online';
